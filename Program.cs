827336d61834800433ed1360300c23d06ac0f780
Jarvis-69 <94354305+Jarvis-69@users.noreply.github.com>
2023-01-02 16:59:53 +0100

    Exo8

--- Program.cs
+++ Program.cs
@@ -7,33 +7,18 @@ namespace ConsoleApp
     {
         public static void Main(string[] args)
         {
-            WriteLine ("Entre un Nombre");
-            int Nb1 = Convert.ToInt32(ReadLine());
-            WriteLine ("Entre un Nombre");
-            int Nb2 = Convert.ToInt32(ReadLine());
-            WriteLine ("Entre un Nombre");
-            int Nb3 = Convert.ToInt32(ReadLine());
-            WriteLine ("Entre un Nombre");
-            int Nb4 = Convert.ToInt32(ReadLine());
-            WriteLine ("Entre un Nombre");
-            int Nb5 = Convert.ToInt32(ReadLine());
-            WriteLine ("Entre un Nombre");
-            int Nb6 = Convert.ToInt32(ReadLine());
-            WriteLine ("Entre un Nombre");
-            int Nb7 = Convert.ToInt32(ReadLine());
-            WriteLine ("Entre un Nombre");
-            int Nb8 = Convert.ToInt32(ReadLine());
-            WriteLine ("Entre un Nombre");
-            int Nb9 = Convert.ToInt32(ReadLine());
-            WriteLine ("Entre un Nombre");
-            int Nb10 = Convert.ToInt32(ReadLine());
+            int [] Tableau = new int [10];
 
-            int [] Tableau = new int [10] {Nb1,Nb2,Nb3,Nb4,Nb5,Nb6,Nb7,Nb8,Nb9,Nb10};
+            for (int i = 0; i < Tableau.Length; i++)
+            {
+                WriteLine($"Entrez un nombre ({i + 1}/10) :");
+                Tableau[i] = Convert.ToInt32(ReadLine());
+            }
 
-           for (int i = 0; i < Tableau.Length; i++)
-           {
-                Write (Tableau[i] + " ");
-           }
+            foreach (int nombre in Tableau)
+            {
+                Write(nombre + " ");
+            }
         }
     }
 }
