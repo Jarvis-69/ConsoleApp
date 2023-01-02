bddb95c4de06d50a3c3fd3ff9f1e21974444fe51
Jarvis-69 <94354305+Jarvis-69@users.noreply.github.com>
2023-01-02 16:54:46 +0100

    Exo7

--- Program.cs
+++ Program.cs
@@ -7,7 +7,28 @@ namespace ConsoleApp
     {
         public static void Main(string[] args)
         {
-           int [] Tableau = new int [10] {1,2,3,4,5,6,7,8,9,10};
+            WriteLine ("Entre un Nombre");
+            int Nb1 = Convert.ToInt32(ReadLine());
+            WriteLine ("Entre un Nombre");
+            int Nb2 = Convert.ToInt32(ReadLine());
+            WriteLine ("Entre un Nombre");
+            int Nb3 = Convert.ToInt32(ReadLine());
+            WriteLine ("Entre un Nombre");
+            int Nb4 = Convert.ToInt32(ReadLine());
+            WriteLine ("Entre un Nombre");
+            int Nb5 = Convert.ToInt32(ReadLine());
+            WriteLine ("Entre un Nombre");
+            int Nb6 = Convert.ToInt32(ReadLine());
+            WriteLine ("Entre un Nombre");
+            int Nb7 = Convert.ToInt32(ReadLine());
+            WriteLine ("Entre un Nombre");
+            int Nb8 = Convert.ToInt32(ReadLine());
+            WriteLine ("Entre un Nombre");
+            int Nb9 = Convert.ToInt32(ReadLine());
+            WriteLine ("Entre un Nombre");
+            int Nb10 = Convert.ToInt32(ReadLine());
+
+            int [] Tableau = new int [10] {Nb1,Nb2,Nb3,Nb4,Nb5,Nb6,Nb7,Nb8,Nb9,Nb10};
 
            for (int i = 0; i < Tableau.Length; i++)
            {
