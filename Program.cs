9c5d3a16e04af5a0502edbc176cc257f224294c9
Jarvis-69 <94354305+Jarvis-69@users.noreply.github.com>
2023-01-02 16:48:57 +0100

    Exo6

--- Program.cs
+++ Program.cs
@@ -7,19 +7,12 @@ namespace ConsoleApp
     {
         public static void Main(string[] args)
         {
-            WriteLine("Choisi un premier nombre");
-            int PremierNombre = Convert.ToInt32(ReadLine());
-            WriteLine("Choisi un deuxieme nombre");
-            int DeuxiemeNombre = Convert.ToInt32(ReadLine());
-            
-            int Produit = PremierNombre * DeuxiemeNombre;
-            while (Produit < 1000)
-            {
-                WriteLine("Choisi un autre nombre");
-                int AutreNombre = Convert.ToInt32(ReadLine());
-                Produit = Produit * AutreNombre;
-            }
-            WriteLine ("Le produit depasse 1000");
+           int [] Tableau = new int [10] {1,2,3,4,5,6,7,8,9,10};
+
+           for (int i = 0; i < Tableau.Length; i++)
+           {
+                Write (Tableau[i] + " ");
+           }
         }
     }
 }
